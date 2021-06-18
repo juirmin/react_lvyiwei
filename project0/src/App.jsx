479d919ff5dfcba18766d452d9c72fa655e28a1c
juirmin <kmes990402081@gmail.com>
2021-06-19 00:23:28 +0800

    2021_04_08

--- project0/src/App.jsx
+++ project0/src/App.jsx
@@ -1,21 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-const App = () => (
-   <div>
-      <Header />
-      <Content />
-   </div>
-);
-const Header = () => (
-   <div>
-      <h1>Header</h1>
-   </div>
-);
-const Content = () => (
-   <div>
-      <h2>Content</h2>
-      <p>The content text!!!</p>
-   </div>
-);
+
+function formatDate(date) {
+   return date.toLocaleDateString();
+}
+
+class Avatar extends React.Component {
+   render() {
+      return (
+         <img
+            className="Avatar"
+            src={this.props.user.avatarUrl}
+            alt={this.props.user.name}
+         />
+      );
+   }
+}
+class UserInfo extends React.Component {
+   render() {
+      return (
+         <div className="UserInfo">
+            <Avatar user={this.props.user} />
+            <div className="UserInfo-name">{this.props.user.name}</div>
+         </div>
+      );
+   }
+}
+class App extends React.Component {
+   render() {
+      return (
+         <div className="Comment">
+            <UserInfo user={this.props.author} />
+            <div className="Comment-text">{this.props.text}</div>
+            <div className="Comment-date">
+               {formatDate(this.props.date)}
+            </div>
+         </div>
+      );
+   }
+}
+
+
+
 export default App;
--- project0/src/index.js
+++ project0/src/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-
-ReactDOM.render(<App />, document.getElementById('app'));
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'https://placekitten.com/g/64/64',
+  },
+};
+ReactDOM.render(
+  <App
+    date={comment.date}
+    text={comment.text}
+    author={comment.author}
+  />,
+  document.getElementById('root')
+);
